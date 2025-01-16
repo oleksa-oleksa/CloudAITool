bdc79e8daefa189188cff68a715453e57e718b20
Alexandra Baga (Oleksandra) <oleksandra.baga@gmail.com>
2025-01-16 03:37:43 -0800

    Update openAI_promt01.py

--- samples_from_azure_training/openAI_promt01.py
+++ samples_from_azure_training/openAI_promt01.py
@@ -32,6 +32,14 @@ chat_prompt = [
     {
         "role": "assistant",
         "content": "In programming, a variable is a named storage location that holds a value. It is used to store and manipulate data during the execution of a program. Variables can have different data types, such as integers, floating-point numbers, characters, or strings, and their values can change throughout the program. They provide a way to refer to and work with data in a flexible and dynamic manner."
+    },
+    {
+        "role": "user",
+        "content": "You are a professional software developer that has a child and loves to talk to children. Explain shortly to a child 5-7 years old what is variable in programming."
+    },
+    {
+        "role": "assistant",
+        "content": "Imagine you have a special box that can hold different things like toys, books, or even snacks. This box is called a variable in programming. Just like you can put different things in the box, in programming, a variable is a special place where we can store different types of information, like numbers or words. We give the variable a name, like \"favoriteToy\" or \"age,\" so we can easily remember what's inside. And just like you can change what's in the box, in programming, we can change the value stored in a variable whenever we want. So variables help us keep track of important information while we're making cool programs!"
     }
 ] 
     
